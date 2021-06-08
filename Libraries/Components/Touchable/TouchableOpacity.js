e8302eef74ef34dda90ddd6f4313c7246362cffa
杨仕航 <624644925@qq.com>
2021-06-08 18:43:09 +0800

    Update TouchableOpacity.js

--- Libraries/Components/Touchable/TouchableOpacity.js
+++ Libraries/Components/Touchable/TouchableOpacity.js
@@ -294,6 +294,7 @@ class TouchableOpacity extends React.Component<Props, State> {
                 this.props.onPress !== undefined,
             })}
         // macOS]
+        cursor={this.props.cursor}
         tooltip={this.props.tooltip} // TODO(macOS/win GH#774)
         onMouseEnter={this.props.onMouseEnter} // [TODO(macOS GH#774)
         onMouseLeave={this.props.onMouseLeave}
